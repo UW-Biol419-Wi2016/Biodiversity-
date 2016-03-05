cb6de0a7b0377713552801afd1b7ab5493e9bc06
D-Mitri <heneric8@uw.edu>
2016-03-04 23:09:52 -0800

    Visualization Refinement
    I refined the plots used to visualize the data to better understand what
    we were working with.

--- BY_FH_biodiv.m
+++ BY_FH_biodiv.m
@@ -13,7 +13,20 @@ cnt = cnt(2:end, 1);
 [hab13, txt13] = habitatRead(num, txt);
 
 %% Look at data
+% this first figure just looks at the distribution of habitats
+figure;
+hold on;
 
+subplot(2,1,1);
+histogram(categorical(txt12));
+title('Habitat Distribution of 2012');
+
+subplot(2,1,2);
+histogram(categorical(txt13));
+title('Habitat Distribution of 2013');
+
+% the second graph compares the relative healths of the habitats
+% the marks are colored based on region
 figure;
 hold on;
 scatter(hab12(:,2), hab12(:,3), 20, hab12(:,1), 'o');
@@ -23,3 +36,14 @@ title('Comparison of Habitat Healths Accross the World');
 xlabel('Habitat');
 ylabel('Health Score (0-1)');
 legend('2012', '2013', 'Location', 'Best');
+
+% the last figure compares the healths between 2012 and 2013
+% the added line is to better show deviations changes
+figure;
+hold on;
+plot([0,1],[0,1]);
+plot(hab12(:,3), hab13(:,3), 'r.');
+
+title('Change in Habitat Healths from 2012 to 2013');
+xlabel('2012 Habitat Health');
+ylabel('2013 Habitat Health');
